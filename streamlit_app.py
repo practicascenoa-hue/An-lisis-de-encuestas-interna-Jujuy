73dc04907bff17137fe8c06403d859fcc0184e4b
practicascenoa-hue <practicas.cenoa@gmail.com>
2026-06-09 21:26:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -252,6 +252,7 @@ if df_raw is not None:
                # --- 2. SECCIÓN INFERIOR: CUADRÍCULA DE ANILLOS LIMPIOS ---
                st.markdown("### Detalle por Pregunta de la Encuesta (Estilo Corporativo)")
                
+               # --- FILA 1 DE ANILLOS (Preguntas 1, 2 y 3) ---
                cod1, cod2, cod3 = st.columns(3)
                with cod1:
                     col_f_turno = df_mes.columns[5]
@@ -261,9 +262,8 @@ if df_raw is not None:
                     st.plotly_chart(crear_anillo_corporativo(df_mes[col_h_asesor], "Q8 - Cortesía y Competencia Asesor"), use_container_width=True, key="anillo_q8_asesor")
                with cod3:
                     st.plotly_chart(crear_anillo_corporativo(df_mes[col_ambiente_J], "Q6 - Calidad Instalaciones y Confort"), use_container_width=True, key="anillo_q6_ambiente")
-     
-               st.write("")
                
+               st.write("")
                cod4, cod5, cod6 = st.columns(3)
                with cod4:
                     col_l_chapa = df_mes.columns[11]
